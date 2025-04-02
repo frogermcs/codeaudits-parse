51eecbc7c2096765fa9449dfc8a1ad1a40617a53
Mirosław Stanek <froger.mcs@gmail.com>
2025-04-02 11:14:05 +0200

    add working directory to input params

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core'
 import { CliOptions, runCli } from 'repomix'
 import fs from 'node:fs/promises'
+import path from 'node:path'
 import { submitToCodeAudits } from './codeaudits-submission.js'
 
 /**
@@ -14,10 +15,13 @@ export async function run(): Promise<void> {
     const compress: boolean = core.getBooleanInput('compress')
     const pushToCodeaudits: boolean = core.getBooleanInput('push-to-codeaudits')
     const outputFilePath = `parsed-repo.txt`
+    const workingDirectory: string = core.getInput('working-directory')
 
     core.debug(
-      `Parsing codebase into ${outputFilePath} file, with style ${style}`
+      `Parsing codebase into ${outputFilePath} file, with style ${style} in directory ${workingDirectory}`
     )
+
+    const absoluteWorkingDirectory = path.resolve(process.cwd(), workingDirectory);
 
     const cliOptions = {
       output: outputFilePath,
@@ -30,7 +34,7 @@ export async function run(): Promise<void> {
       compress: compress
     } as CliOptions
 
-    const result = await runCli(['.'], process.cwd(), cliOptions)
+    const result = await runCli(['.'], absoluteWorkingDirectory, cliOptions)
     if (!result) {
       core.setFailed('Repository could not be parsed')
       return
@@ -51,7 +55,8 @@ export async function run(): Promise<void> {
       .addTable([
         ['Parsed output file', outputFilePath],
         ['Style', style],
-        ['Compress', compress.toString()]
+        ['Compress', compress.toString()],
+        ['Working Directory', workingDirectory]
       ])
       .addBreak()
       .addHeading('Parsed Metadata', 2)
@@ -61,7 +66,7 @@ export async function run(): Promise<void> {
       core.info('Submitting to CodeAudits')
       const apiKey = core.getInput('codeaudits-api-key')
       const basePath = core.getInput('codeaudits-base-path')
-      const content = await fs.readFile(outputFilePath, 'utf-8')
+      const content = await fs.readFile(path.join(absoluteWorkingDirectory, outputFilePath), 'utf-8')
       
       const topFiles = 
         Object.entries(packResult.fileTokenCounts)
